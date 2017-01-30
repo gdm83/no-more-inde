5f983210e80bc8be1e732481dcb985e121420dc1
Cristian <c.bertuccioli@nextsw.it>
2017-01-30 16:12:37 +0100

    first commit cont

---
+++ src/main/java/it/nextsw/no/more/inde/NoMoreIndeApplication.java
@@ -0,0 +1,12 @@
+package it.nextsw.no.more.inde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMoreIndeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoMoreIndeApplication.class, args);
+	}
+}
