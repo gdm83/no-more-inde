cb4aa5e5de66710aec2820ba1068c97cb1a8e51e
user <user@172.26.18.178>
2017-02-01 15:01:16 +0100

    cancellato vecchio main

--- src/main/java/it/bds/maipiuinde/entities/Utente.java
+++ src/main/java/it/bds/maipiuinde/entities/Utente.java
@@ -12,13 +12,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import org.springframework.util.StringUtils;
 
 /**
  *
